cd376d16a3f86d1d2ae57f6d6a199ef8dee010de
DelfiDorussen <116188334+DelfiDorussen@users.noreply.github.com>
2024-07-24 15:19:46 +0100

    Update computeMatrix_5D.sh

--- WGBS/computeMatrix_5D.sh
+++ WGBS/computeMatrix_5D.sh
@@ -13,7 +13,7 @@ source package 6daf0c37-1c5e-4cd6-9884-2d0b4d5f9d8f
 cd /jic/scratch/groups/Philippa-Borrill/Delfi/MET1_WGBS/filter_by_chrom/bigwig_files/
 
 computeMatrix scale-regions \
--S Z0001_Chr5D_CG.bw Z0003_Chr5D_CG.bw Z0007_Chr5D_CG.bw Z0009_Chr5D_CG.bw \
+-S Z0001_Chr5D_CG.bw Z0003_Chr5D_CG.bw Z0007_Chr5D_CG.bw Z0009_Chr5D_CG.bw \ #Change bigwig file input eg for different sequence contexts
 -R /jic/scratch/groups/Philippa-Borrill/References/iwgsc_ref_seq_1.1/iwgsc_refseqv1.1_genes_2017July06/IWGSC_v1.1_HC_20170706_v2_chr5D.gtf \
 -o /jic/scratch/groups/Philippa-Borrill/Delfi/MET1_WGBS/filter_by_chrom/average_methylation/gene_matrix_5D.gz \
 -m 2000 \
